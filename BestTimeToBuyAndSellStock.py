8e447fd797c4eeaf0664115aea97bf26510abeb6
aakriti208 <aakritidh208@gmail.com>
2024-11-16 13:23:23 -0600

    Solution and explanation : BestTimeToBuyAndSellStock

---
+++ BestTimeToBuyAndSellStock.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, prices):
+        # First we assign a mininum price, which is infinity
+        min_price = float(inf)
+        # We assign the maximum profit we have so far, which is at 0
+        max_profit = 0
+
+        # We go through each price in the list
+        for price in prices:
+            # If any of the price is less than the minimum price we have, we replace the minimum price with the current price
+            if price < min_price:
+                min_price = price
+
+            # We calculate the profit, which is the difference between the current price we're looking at and the minimum price we have
+            profit = price - min_price
+
+            # We check to see if the profit we got is greater than the maximim profit we currently have, and replace it
+            if profit > max_profit:
+                max_profit = profit
+            
+        return max_profit
