c06a852d7d6b2eaa063a3094c87d7a40f7f82b06
aakriti208 <aakritidh208@gmail.com>
2024-11-30 09:44:30 -0600

    Solution 2 : Valid palindrome

--- validPalindrome.py
+++ validPalindrome.py
@@ -15,3 +15,13 @@ class Solution:
         return (ord("A") <= ord(c) <= ord("Z") or
                 ord("a") <= ord(c) <= ord("z") or
                 ord("0") <= ord(c) <= ord("9") )
+    
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newStr = ""
+        for c in s:
+            if c.isalnum():
+                newStr += c.lower()
+        return newStr == newStr[::-1]
