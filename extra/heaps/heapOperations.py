2b049d3ad9c33cfcba9478691bf12377123a7df3
aakriti208 <aakritid208@gmail.com>
2025-02-27 14:51:24 -0600

    Heap operations

---
+++ extra/heaps/heapOperations.py
@@ -0,0 +1,30 @@
+import heapq
+
+# Inserting an element into the heap : O(logn)
+# Add the element at the end and bubble up to restore heap property
+
+heap = []  # Min-Heap
+heapq.heappush(heap, 10)
+heapq.heappush(heap, 5)
+heapq.heappush(heap, 20)
+heapq.heappush(heap, 12)
+heapq.heappush(heap, 55)
+heapq.heappush(heap, 12)
+heapq.heappush(heap, 3)
+print(heap)  
+
+# Remove/Exstract the minimum element from the list
+min_element = heapq.heappop(heap)
+print(min_element)
+
+# Peek at the top or the minimum element in the list
+print(heap[0])
+
+# Build heap from an array(heapify)
+# O(n) It's faster than inserting one-by-one
+arr = [40,30,60,20,10]
+heapq.heapify(arr)
+print(arr)
+
+
+
