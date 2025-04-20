b72107580e2f51ae8b0ef2507fdae883899ffb61
aakriti208 <aakritid208@gmail.com>
2025-04-20 13:13:12 -0500

    Mininum size subarray sum code

---
+++ minimumSizeSubarraySum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minSubarraySum(self, nums, target):
+        l, total = 0, 9
+        result = float("inf")
+        for r in range(len(nums)):
+            total += nums[r]
+            while total >= target:
+                result = min(r-l+1, result)
+                total -= nums[l]
+                l += 1
+        
+        return 0 if result == float("inf") else result
