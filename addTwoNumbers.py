b817a55ae4e6d1fa59effc9415d0442b19529f50
aakriti208 <aakritid208@gmail.com>
2025-06-04 12:25:01 -0500

    Add two linkedlist numbers explanation

--- addTwoNumbers.py
+++ addTwoNumbers.py
@@ -27,3 +27,14 @@ class Solution:
             
             
         
+# make a dummy node to store the new node of sum
+# the current pointer will point to the dummy node
+# we need to maintain a carry, which will be initialized to 0
+# while the list l1 and l2 are not empty or carry is also not empty (8+7=15), we might have a number like this
+# value 1 is gonna be value of list 1 if present else will be 0, same for value 2
+# for the new digits, we add v1, v2 and carry 
+# the carry will be remainder of value and value will be the mod of the value
+# now that we have the new digit, we can insert it in our list. we're gonna insert a new listnode with the value that we just computed
+# finally we upate all the pointers
+ # we're gonna return dummy.next, that is the list that we just created 
+ 
