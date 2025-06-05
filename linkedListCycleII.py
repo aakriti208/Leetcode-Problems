7d4f04914de33935e2705a760687adb1ea7f8ffd
aakriti208 <aakritid208@gmail.com>
2025-06-05 12:52:44 -0500

    Linked list cycle II explanation

--- linkedListCycleII.py
+++ linkedListCycleII.py
@@ -4,10 +4,17 @@ class Solution:
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
+            # detect a cycle using tortoise and hare solution
             if slow == fast:
                 break
         else: return None
-        while head != fast:
-            head = head.next
+        
+        # finding the starting point
+        # take the pointer slow to the head of the linked list to check where they collide
+        # move slow and fast by one steps as long as they dont collide
+        # the point they collide or meet is the starting point...it could return either slow or fast any
+        slow = head
+        while slow != fast:
+            slow = slow.next
             fast = fast.next
-        return head
+        return slow
