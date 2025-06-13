6acfa722802b129077ab6e2f9a1bc3b369a9b97f
aakriti208 <aakritid208@gmail.com>
2025-06-13 12:57:00 -0500

    Find the minimum in rotated sorted array code

--- searchInRotatedSortedArrayII.py
+++ searchInRotatedSortedArrayII.py
@@ -5,6 +5,8 @@ class Solution:
             mid = (l + r) // 2
             if nums[mid] == target:
                 return True
+            # in this case we have to check the edge case, when 3 elements could be the same
+            # since the condition of checking in left or right sorted portion does not work
             if nums[l] == nums[mid] == nums[r]:
                 l += 1
                 r -= 1
@@ -23,3 +25,5 @@ class Solution:
         return False
 
         
+    # avg t.c : O(log n)
+    # worst t.c : O(n/2) - since we have to shrink the input data 
