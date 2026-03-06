0a39d9bcf53db34ab6520b7babc5387774fd180d
aakriti208 <aakritid208@gmail.com>
2026-03-05 18:08:09 -0600

    rev

--- topKFrequentElements.py
+++ topKFrequentElements.py
@@ -22,4 +22,18 @@ class Solution:
 
 
 
-        
+        n = len(nums)
+        counter = Counter(nums)
+        buckets = [0]*(n+1)
+        for num, freq in counter.items():
+            if buckets[freq] == 0:
+                buckets[freq] = [num]
+            else:
+                buckets[freq].append(num)
+                
+        res = []
+        for i in range(n, -1, -1):
+            if buckets[i] != 0:
+                res.extend(buckets[i])
+            if len(res) == k:
+                return res
