e52329358931736d4355d6be8484ea9e2ef747cf
aakriti208 <aakritid208@gmail.com>
2026-03-08 10:45:04 -0500

    Number of islands solution

---
+++ numberOfIslands.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def numIslands(self, grid):
+        r, c = len(grid), len(grid[0])
+
+        def dfs(i, j):
+            if i<0 or i>=r or j<0 or j>=c or grid[i][j] != '1':
+                return
+            else:
+                grid[i][j] = '0'
+                dfs(i, j+1)
+                dfs(i+1, j)
+                dfs(i-1, j)
+                dfs(i, j-1)
+
+        numOfIslands = 0
+        for i in range(r):
+            for j in range(c):
+                if grid[i][j] == '1':
+                    numOfIslands += 1
+                    dfs(i, j)
+        return numOfIslands
+        
+         
+        
